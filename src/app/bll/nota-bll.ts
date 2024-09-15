3798bd95c41f673e592ac46db31ddea879ddc726
Diyhi0515 <120338163+Diyhi0515@users.noreply.github.com>
2024-09-14 23:28:37 -0400

    Refactorizar NotaBLL y DbService para mejorar el manejo de la base de datos

--- src/app/bll/nota-bll.ts
+++ src/app/bll/nota-bll.ts
@@ -1,2 +1,65 @@
+import { DbService } from "../services/db.service";
+
 export class NotaBLL {
+    public async selectAll(dbService: DbService) {
+        if(!dbService.database){
+            await dbService.createDb();
+        }
+
+        const sSelect = 'SELECT * FROM notas';
+        return await dbService.database?.executeSql(sSelect, [])
+        .catch((error) => {
+            console.log('No se pudo seleccionar Notas', error);
+        });
+    }
+    
+    public async getNotaById(dbService: DbService, id: number) {
+        if(!dbService.database){
+            await dbService.createDb();
+        }
+
+        const sSelect = 'SELECT * FROM notas WHERE id = ?';
+        return await dbService.database?.executeSql(sSelect, [id])
+        .catch((error) => {
+            console.log('No se pudo seleccionar Nota con ID', error);
+        });
+    }
+
+    public async insertNota(dbService: DbService, texto: string) {
+        if(!dbService.database){
+            await dbService.createDb();
+        }
+
+        const sInsert = 'INSERT INTO notas (texto) VALUES (?)';
+        return await dbService.database?.executeSql(sInsert, [texto])
+        .catch((error) => {
+            console.log('No se pudo insertar Nota', error);
+        });
+    }
+
+    public async updateNota(dbService: DbService, id: number, texto: string) {
+        if(!dbService.database){
+            await dbService.createDb();
+        }
+
+        const sUpdate = 'UPDATE notas SET texto = ? WHERE id = ?';
+        return await dbService.database?.executeSql(sUpdate, [texto, id])
+        .catch((error) => {
+            console.log('No se pudo actualizar Nota', error);
+        });
+    }
+
+    public async deleteNota(dbService: DbService, id: number) {
+        if(!dbService.database){
+            await dbService.createDb();
+        }
+
+        const sDelete = 'DELETE FROM notas WHERE id = ?';
+        return await dbService.database?.executeSql(sDelete, [id])
+        .catch((error) => {
+            console.log('No se pudo eliminar Nota', error);
+        });
+    }
+
+
 }
--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Platform } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbService {
 
-  constructor() { }
+  public database?: SQLiteObject;
+
+  constructor(private sqlite: SQLite, private platform: Platform) { 
+    this.platform.ready().then(() => {
+      this.createDb();
+    });
+  }
+
+  async createDb() {
+    await this.sqlite.create({
+      name: 'notas.db',
+      location: 'default'
+    })
+    .then((db: SQLiteObject) => {
+      this.database = db;
+      this.createTable();
+    })
+    .catch((error) => {
+      console.log('Erro ao criar o banco de dados', error);
+    });
+
+  }
+
+  async createTable() {
+    const sqlTable = 'CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, texto TEXT)';
+    await this.database?.executeSql(sqlTable, [])
+    .then(() => {
+      console.log('Tabela creada con exito');
+    })
+    .catch((error) => {
+      console.log('Error al crear tabla', error);
+    });
+  }
+  
 }
