987f093fbb430f7ca630bfd719ff756d5628bec0
Diyhi0515 <120338163+Diyhi0515@users.noreply.github.com>
2024-09-14 22:27:49 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Practico2_Notas',
+  webDir: 'www'
+};
+
+export default config;
